741d624f5f2f0fa2265e79fa1d4d5269c8f5f794
Dibakar Dey <110836157+dibakar63@users.noreply.github.com>
2023-04-25 15:47:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -45,8 +45,8 @@ app.post("/checkout", async (req, res) => {
     const session = await stripe.checkout.sessions.create({
         line_items: lineItems,
         mode: 'payment',
-        success_url:  "https://thunderous-nasturtium-64dc45.netlify.app/success",
-        cancel_url:  "https://thunderous-nasturtium-64dc45.netlify.app/cancel"
+        success_url:  "/success",
+        cancel_url:  "/cancel"
     });
 
     res.send(JSON.stringify({
